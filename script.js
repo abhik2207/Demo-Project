a6c0e7311c5471855b4464fe64a25e2738ef7500
Abhik <abhikgupta01@gmail.com>
2023-10-14 01:19:37 +0530

    Third commit

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ document.getElementById('btn').addEventListener('click', () => {
     document.getElementById('para').innerHTML = 'My name is Abhik';
     const a = 5;
     const b = 10;
-    console.log(a + b);
+    console.log(a*b);
 });
