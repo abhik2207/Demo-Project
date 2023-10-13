1f1e40ef7c00779a46080d65c49fdd7d0caed656
Abhik <abhikgupta01@gmail.com>
2023-10-13 12:15:59 +0530

    Changed files

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-document.getElementById('btn').addEventListener('click', ()=>{
+document.getElementById('btn').addEventListener('click', () => {
     document.getElementById('para').innerHTML = 'My name is Abhik';
+    const a = 5;
+    const b = 10;
+    console.log(a + b);
 });
